cc961a78db3f06d4be65658c18fccb9fb595df3d
paria03 <paria03@outlook.com>
2021-02-24 22:16:39 -0500

    table modified

--- src/App/components/CoinModule/index.js
+++ src/App/components/CoinModule/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
+const tableStyles = {
+	color: "white",
+  backgroundColor: "DodgerBlue",
+	padding: "10px",
+	fontFamily: "Arial",
+	marginLeft: "auto",
+	marginRight: "auto",
+	borderRadius: "14px",
+	width: "80%",
+	boxShadow: "-5px -5px 5px #888888"
+}
+
 export default function CoinModule() {
   return (
 		<>
-			<h2 className="header">CoinModule</h2> 
+			<table style={tableStyles}>
+				<tr>
+					<th>Firstname</th>
+					<th>Lastname</th>
+					<th>Age</th>
+				</tr>
+				<tr>
+					<td>Jill</td>
+					<td>Smith</td>
+					<td>50</td>
+				</tr>
+				<tr>
+					<td>Eve</td>
+					<td>Jackson</td>
+					<td>94</td>
+				</tr>
+			</table>
 		</>
   );
 }
