7a442d74bd90d4deeec5614100c9220b4d28d6d1
paria03 <paria03@outlook.com>
2021-02-27 21:06:33 -0500

    header css update

--- src/App/components/CoinModule/index.js
+++ src/App/components/CoinModule/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const tableStyles = {
-	color: "white",
-  backgroundColor: "DodgerBlue",
-	padding: "10px",
+	color: "black",
+	marginTop: "100px",
+	paddingTop: "20px",
+	paddingBottom: "20px",
 	fontFamily: "Arial",
 	marginLeft: "auto",
 	marginRight: "auto",
@@ -12,9 +13,32 @@ const tableStyles = {
 	boxShadow: "-5px -5px 5px #888888"
 }
 
+const centerText = {
+    textAlign: "center", 
+    verticalAlign: "middle"
+}
+
 export default function CoinModule() {
   return (
 		<>
+			<table style={tableStyles}>
+				<tr>
+					<th style={centerText}>Firstname</th>
+					<th style={centerText}>Lastname</th>
+					<th style={centerText}>Age</th>
+				</tr>
+				<tr>
+					<td style={centerText}>Jill</td>
+					<td style={centerText}>Smith</td>
+					<td style={centerText}>50</td>
+				</tr>
+				<tr>
+					<td style={centerText}>Eve</td>
+					<td style={centerText}>Jackson</td>
+					<td style={centerText}>94</td>
+				</tr>
+			</table>
+
 			<table style={tableStyles}>
 				<tr>
 					<th>Firstname</th>
--- src/App/components/Header/index.js
+++ src/App/components/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './index.css';
 
 export default function Header() {
   return (
 		<>
-			<h1 className="header">CRYPTOPOLIS</h1> 
+			<h1 className="main-title"> CRYPTOPOLIS </h1> 
 		</>
   );
 }
