0075cb4cf74762dce176c4c6abdfe3275482d4b2
paria03 <paria03@outlook.com>
2021-03-10 23:11:21 -0500

    particles.js working

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/index.js';
 import CoinModule from './components/CoinModule/index.js';
+//import Background from './components/Background/index.js';
 
 function App() {
   return (
---
+++ src/App/components/Background/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+//import './particles.js';
+//import './app.js';
+
+export default function CoinModule() {
+  return (
+		<>
+			<div id="particle-div"></div>
+		</>
+  );
+}
+
