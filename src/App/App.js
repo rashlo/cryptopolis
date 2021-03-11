9a567c47416d9ed9dede35ba1efd6d1a3bda1908
paria03 <paria03@outlook.com>
2021-03-11 01:56:38 -0500

    background particles.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/Header/index.js';
 import CoinModule from './components/CoinModule/index.js';
-//import Background from './components/Background/index.js';
 
 function App() {
   return (
